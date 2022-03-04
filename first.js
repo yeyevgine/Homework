0e6265f5e39c312eecad66165af0fe65de2cc7cc
yevginemnatsakanyan <evgine.v.mnatsakanyan@gmail.com>
2022-03-05 01:17:20 +0400

    Update first.js

--- first.js
+++ first.js
@@ -1,14 +1,20 @@
 //1. Տրված N բնական թվի համար հաշվել այդ թվի ֆակտորիալը  N! = 1 * 2 * …* N 
-/*if (num == 0) {
-   return 1;
- } else if (num < 0) {
-   return 0
- }
- return (num * fac(num - 1));*/
+/*
+function Factorial(num) {
+   if (num == 0) {
+    return 1;
+   } else if (num < 0) {
+     return 0
+   }
+   return (num * fac(num - 1));
+}
+*/
 
-function fac(num) {
+/*
+function Factorial(num) {
 
-  return (num == 0) ? 1 : (num < 0) ? 0 : (num * fac(num - 1));
+  return (num == 0) ? 1 : (num < 0) ? 0 : (num * Factorial(num - 1));
    
 }
-console.log(fac(1));
+console.log(Factorial(1));
+*/
